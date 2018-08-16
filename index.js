98733a6fec51296f47cd6db281e79148132da296
Travis Fischer <fisch0920@gmail.com>
2018-08-15 23:59:54 -0500

    .

---
+++ index.js
@@ -0,0 +1,232 @@
+'use strict'
+
+const ow = require('ow')
+const request = require('request-promise-native')
+
+const projects = require('./lib/projects')
+
+const domainToSuffix = {
+  usdo: 'usdo',
+  vndo: 'vndo',
+  'do': 'do',
+  usa: 'usdo',
+  us: 'usdo',
+  china: 'do',
+  hk: 'do',
+  cn: 'do',
+  'asia': 'vndo',
+  'seasia': 'vndo'
+}
+
+const countryCodes = new Set([
+  'my', // Malaysia
+  'ph', // Philippines
+  'kh', // Cambodia
+  'mm', // Myanmar
+  'vn', // Vietnam
+  'id', // Indonesia
+  'hk', // Hong Kong
+  'mo', // Macao
+  'th', // Thailand
+  'br', // Brazil
+  'eg', // Egypt
+  'za' // South Africa
+])
+
+/**
+ * @class GetSMSCodeClient
+ *
+ * @param {object} [opts] - Config options
+ * @param {string} [opts.username=process.env.GETSMSCODE_USERNAME] - Username for wahlburger auth
+ * @param {string} [opts.token=process.env.GETSMSCODE_TOKEN] - Token for wahlburger auth
+ * @param {string} [opts.domain='china'] - Domain for this client to use (china/usa/asia)
+ */
+class GetSMSCodeClient {
+  constructor (opts = { }) {
+    let {
+      username = process.env.GETSMSCODE_USERNAME,
+      token = process.env.GETSMSCODE_TOKEN,
+      domain = 'china',
+      cocode
+    } = opts
+
+    ow(username, ow.string.nonEmpty.label('username'))
+    ow(token, ow.string.nonEmpty.label('token'))
+    ow(domain, ow.string.nonEmpty.label('domain'))
+    ow(opts, ow.object.plain.label('opts'))
+
+    if (cocode) {
+      domain = 'asia'
+      this._cocode = cocode
+      if (!countryCodes.has(cocode)) throw new Error(`invalid cocode "${cocode}"`)
+    }
+
+    this._token = token
+    this._username = username
+
+    const suffix = domainToSuffix[domain]
+    if (!suffix) throw new Error(`unknown wahlburger domain "${domain}"`)
+
+    this._url = `http://www.wahlburger.com/${suffix}.php`
+    this._isCountryCodeRequired = (suffix === 'vndo')
+  }
+
+  /**
+   * Logs in to test auth and fetches an account summary.
+   *
+   * @return {Promise}
+   */
+  async login () {
+    const result = await this._request('login')
+
+    const [ username, balance, points ] = result.split('|')
+    return {
+      username,
+      balance,
+      points
+    }
+  }
+
+  /**
+   * Acquires a temporary handle on a mobile number usable for the given service.
+   *
+   * You must specify either `opts.service` or `opts.pid`.
+   *
+   * @param {object} opts - Config options
+   * @param {string} [opts.service] - Name of service to blacklist number
+   * @param {string} [opts.pid] - Project ID of service to blacklist number
+   * @param {string} [opts.cocode] - Country code (required if using asian domain)
+
+   * @return {Promise}
+   */
+  async getNumber (opts) {
+    const {
+      service,
+      pid = projects.serviceToPID[service.toLowerCase()],
+      cocode
+    } = opts
+
+    if (!pid) throw new Error(`unrecognized service "${service}"`)
+    if (this._isCountryCodeRequired) {
+      if (!this._cocode && !cocode) throw new Error('cocode required')
+      if (cocode && !countryCodes.has(cocode)) throw new Error(`invalid cocode "${cocode}"`)
+    }
+
+    const result = await this._request('getmobile', { pid, cocode })
+
+    if (result.indexOf('|') >= 0) {
+      throw new Error(result)
+    }
+
+    return result
+  }
+
+  /**
+   * Returns a list of `{ number, service }` objects currently in use by this account.
+   *
+   * @return {Promise}
+   */
+  async getNumbers () {
+    const numbers = await this._request('mobilelist')
+
+    return numbers.split(',')
+      .map((o) => {
+        const [ number, pid ] = o.split('|')
+        const service = projects.pidToService[pid]
+
+        return { number, service }
+      })
+  }
+
+  /**
+   *
+   * You must specify either `opts.service` or `opts.pid`.
+   *
+   * @param {object} opts - Config options
+   * @param {string} opts.number - Mobile number to blacklist
+   * @param {string} [opts.service] - Name of service to blacklist number
+   * @param {string} [opts.pid] - Project ID of service to blacklist number
+   * @param {string} [opts.cocode] - Country code (required if using asian domain)
+
+   * @return {Promise}
+   */
+  async getSMS (opts) {
+    const {
+      number,
+      service,
+      pid = projects.serviceToPID[service.toLowerCase()],
+      cocode
+    } = opts
+
+    if (!pid) throw new Error(`unrecognized service "${service}"`)
+    if (this._isCountryCodeRequired) {
+      if (!this._cocode && !cocode) throw new Error('cocode required')
+      if (cocode && !countryCodes.has(cocode)) throw new Error(`invalid cocode "${cocode}"`)
+    }
+
+    const result = await this._request('getsms', { mobile: number, pid, cocode })
+
+    if (result.startsWith('1|')) {
+      return result.slice(2)
+    }
+
+    return result
+  }
+
+  /**
+   * Adds a number to this account's blacklist for the given service.
+   *
+   * You must specify either `opts.service` or `opts.pid`.
+   *
+   * @param {object} opts - Config options
+   * @param {string} opts.number - Mobile number to blacklist
+   * @param {string} [opts.service] - Name of service to blacklist number
+   * @param {string} [opts.pid] - Project ID of service to blacklist number
+   * @param {string} [opts.cocode] - Country code (required if using asian domain)
+   *
+   * @return {Promise}
+   */
+  async addNumberToBlacklist (opts) {
+    const {
+      number,
+      service,
+      pid = projects.serviceToPID[service.toLowerCase()],
+      cocode
+    } = opts
+
+    if (!pid) throw new Error(`unrecognized service "${service}"`)
+    if (this._isCountryCodeRequired) {
+      if (!this._cocode && !cocode) throw new Error('cocode required')
+      if (cocode && !countryCodes.has(cocode)) throw new Error(`invalid cocode "${cocode}"`)
+    }
+
+    return this._request('getsms', { mobile: number, pid, cocode })
+  }
+
+  _request (action, params = { }) {
+    if (!this._isCountryCodeRequired) {
+      delete params.cocode
+    }
+
+    const qs = {
+      action,
+      username: this._username,
+      token: this._token,
+      ...params
+    }
+
+    if (this._cocode) {
+      qs.cocode = qs.cocode || this._cocode
+    }
+
+    console.log('POST', this._url, JSON.stringify(qs, null, 2))
+
+    return request({
+      method: 'POST',
+      url: this._url,
+      qs
+    })
+  }
+}
+
+module.exports = GetSMSCodeClient
---
+++ readme.md
@@ -0,0 +1,37 @@
+# wahlburger
+
+> Get dem burgers.
+
+[![NPM](https://img.shields.io/npm/v/wahlburger.svg)](https://www.npmjs.com/package/wahlburger) [![Build Status](https://travis-ci.com/transitive-bullshit/wahlburger.svg?branch=master)](https://travis-ci.com/transitive-bullshit/wahlburger) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+![](./wahlburger.jpg)
+
+## Install
+
+This module requires `node >= 8`.
+
+```bash
+npm install --save wahlburger
+```
+
+You'll need to setup a [wahlburger.com](http://www.wahlburger.com/) account and add some money to your account before using this module.
+
+## Usage
+
+...
+
+## API
+
+
+## Related
+
+-   [sms-number-verifier](https://github.com/transitive-bullshit/sms-number-verifier) - Allows you to spoof SMS number verification.
+-   [parse-otp-message](https://github.com/transitive-bullshit/parse-otp-message) - Parses OTP messages for a verification code and service provider.
+
+## Disclaimer
+
+Using this software to violate the terms and conditions of any third-party service is strictly against the intent of this software. By using this software, you are acknowledging this fact and absolving the author or any potential liability or wrongdoing it may cause. This software is meant for testing and experimental purposes only, so please act responsibly.
+
+## License
+
+MIT © [Travis Fischer](https://github.com/transitive-bullshit)
